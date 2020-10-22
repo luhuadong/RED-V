9521a9ad39f400c623d239b43d644ddb062f7636
luhuadong <luhuadong@163.com>
2020-10-23 01:18:43 +0800

    add red-v-oled

---
+++ code/red-v-oled/src/example-i2c.c
@@ -0,0 +1,311 @@
+/* Copyright 2019 SiFive, Inc */
+/* SPDX-License-Identifier: Apache-2.0 */
+
+#include <metal/cpu.h>
+#include <metal/i2c.h>
+#include <metal/io.h>
+#include <metal/machine.h>
+#include <metal/uart.h>
+#include <stdio.h>
+#include <time.h>
+
+#include "codetab.h"
+
+//#define OLED_I2C_ADDRESS	0x78
+#define OLED_I2C_ADDRESS	0x3C
+#define I2C_BAUDRATE        100000
+
+#define RET_OK    0
+#define RET_NOK   1
+
+/* 1s delay macro */
+#define WAIT_1S(timeout)                                                       \
+  timeout = time(NULL) + 1;                                                    \
+  while (timeout > time(NULL))                                                 \
+    ;
+
+void delay_ms(int ms)
+{
+	clock_t begin = clock();
+
+	while ((clock() - begin) < (ms * 1000));
+}
+
+void DelayMs(int ms)
+{
+	clock_t begin = clock();
+
+	while ((clock() - begin) < (ms * 1000));
+}
+
+static struct metal_i2c *i2c_bus;
+
+/* function */
+void I2C_WriteByte(uint8_t addr,uint8_t data);
+void WriteCmd(unsigned char I2C_Command);
+void WriteDat(unsigned char I2C_Data);
+void OLED_Init(void);
+void OLED_SetPos(unsigned char x, unsigned char y);
+void OLED_Fill(unsigned char fill_Data);
+void OLED_CLS(void);
+void OLED_ON(void);
+void OLED_OFF(void);
+void OLED_ShowStr(unsigned char x, unsigned char y, unsigned char ch[], unsigned char TextSize);
+void OLED_ShowCN(unsigned char x, unsigned char y, unsigned char N);
+void OLED_DrawBMP(unsigned char x0,unsigned char y0,unsigned char x1,unsigned char y1,unsigned char BMP[]);
+
+//metal_i2c_write(i2c, TEMP_SENSOR_I2C_ADDR, LEN1, buf, METAL_I2C_STOP_DISABLE);
+//metal_i2c_read(i2c, TEMP_SENSOR_I2C_ADDR, LEN1, buf, METAL_I2C_STOP_ENABLE);
+//metal_i2c_transfer(i2c, TEMP_SENSOR_I2C_ADDR, buf, LEN1, buf, LEN2);
+
+void I2C_WriteByte(uint8_t addr,uint8_t data)
+{
+	uint8_t buf[2] = {addr, data};
+	metal_i2c_write(i2c_bus, OLED_I2C_ADDRESS, 2, buf, METAL_I2C_STOP_ENABLE);
+}
+
+void WriteCmd(unsigned char I2C_Command)//д����
+{
+	I2C_WriteByte(0x00, I2C_Command);
+}
+
+void WriteDat(unsigned char I2C_Data)//д����
+{
+	I2C_WriteByte(0x40, I2C_Data);
+}
+
+void OLED_Init(void)
+{
+	DelayMs(100); //�������ʱ����Ҫ
+
+	WriteCmd(0xAE); //display off
+	WriteCmd(0x20);	//Set Memory Addressing Mode
+	WriteCmd(0x10);	//00,Horizontal Addressing Mode;01,Vertical Addressing Mode;10,Page Addressing Mode (RESET);11,Invalid
+	WriteCmd(0xb0);	//Set Page Start Address for Page Addressing Mode,0-7
+	WriteCmd(0xc8);	//Set COM Output Scan Direction
+	WriteCmd(0x00); //---set low column address
+	WriteCmd(0x10); //---set high column address
+	WriteCmd(0x40); //--set start line address
+	WriteCmd(0x81); //--set contrast control register
+	WriteCmd(0xff); //���ȵ��� 0x00~0xff
+	WriteCmd(0xa1); //--set segment re-map 0 to 127
+	WriteCmd(0xa6); //--set normal display
+	WriteCmd(0xa8); //--set multiplex ratio(1 to 64)
+	WriteCmd(0x3F); //
+	WriteCmd(0xa4); //0xa4,Output follows RAM content;0xa5,Output ignores RAM content
+	WriteCmd(0xd3); //-set display offset
+	WriteCmd(0x00); //-not offset
+	WriteCmd(0xd5); //--set display clock divide ratio/oscillator frequency
+	WriteCmd(0xf0); //--set divide ratio
+	WriteCmd(0xd9); //--set pre-charge period
+	WriteCmd(0x22); //
+	WriteCmd(0xda); //--set com pins hardware configuration
+	WriteCmd(0x12);
+	WriteCmd(0xdb); //--set vcomh
+	WriteCmd(0x20); //0x20,0.77xVcc
+	WriteCmd(0x8d); //--set DC-DC enable
+	WriteCmd(0x14); //
+	WriteCmd(0xaf); //--turn on oled panel
+}
+
+void OLED_SetPos(unsigned char x, unsigned char y) //������ʼ������
+{
+	WriteCmd(0xb0+y);
+	WriteCmd(((x&0xf0)>>4)|0x10);
+	WriteCmd((x&0x0f)|0x01);
+}
+
+void OLED_Fill(unsigned char fill_Data)//ȫ�����
+{
+	unsigned char m,n;
+	for(m=0;m<8;m++)
+	{
+		WriteCmd(0xb0+m);		//page0-page1
+		WriteCmd(0x00);		//low column start address
+		WriteCmd(0x10);		//high column start address
+		for(n=0;n<128;n++)
+			{
+				WriteDat(fill_Data);
+			}
+	}
+}
+
+void OLED_CLS(void)//����
+{
+	OLED_Fill(0x00);
+}
+
+//--------------------------------------------------------------
+// Prototype      : void OLED_ON(void)
+// Calls          :
+// Parameters     : none
+// Description    : ��OLED�������л���
+//--------------------------------------------------------------
+void OLED_ON(void)
+{
+	WriteCmd(0X8D);  //���õ�ɱ�
+	WriteCmd(0X14);  //������ɱ�
+	WriteCmd(0XAF);  //OLED����
+}
+
+//--------------------------------------------------------------
+// Prototype      : void OLED_OFF(void)
+// Calls          :
+// Parameters     : none
+// Description    : ��OLED���� -- ����ģʽ��,OLED���Ĳ���10uA
+//--------------------------------------------------------------
+void OLED_OFF(void)
+{
+	WriteCmd(0X8D);  //���õ�ɱ�
+	WriteCmd(0X10);  //�رյ�ɱ�
+	WriteCmd(0XAE);  //OLED����
+}
+
+//--------------------------------------------------------------
+// Prototype      : void OLED_ShowChar(unsigned char x, unsigned char y, unsigned char ch[], unsigned char TextSize)
+// Calls          :
+// Parameters     : x,y -- ��ʼ������(x:0~127, y:0~7); ch[] -- Ҫ��ʾ���ַ���; TextSize -- �ַ���С(1:6*8 ; 2:8*16)
+// Description    : ��ʾcodetab.h�е�ASCII�ַ�,��6*8��8*16��ѡ��
+//--------------------------------------------------------------
+void OLED_ShowStr(unsigned char x, unsigned char y, unsigned char ch[], unsigned char TextSize)
+{
+	unsigned char c = 0,i = 0,j = 0;
+	switch(TextSize)
+	{
+		case 1:
+		{
+			while(ch[j] != '\0')
+			{
+				c = ch[j] - 32;
+				if(x > 126)
+				{
+					x = 0;
+					y++;
+				}
+				OLED_SetPos(x,y);
+				for(i=0;i<6;i++)
+					WriteDat(F6x8[c][i]);
+				x += 6;
+				j++;
+			}
+		}break;
+		case 2:
+		{
+			while(ch[j] != '\0')
+			{
+				c = ch[j] - 32;
+				if(x > 120)
+				{
+					x = 0;
+					y++;
+				}
+				OLED_SetPos(x,y);
+				for(i=0;i<8;i++)
+					WriteDat(F8X16[c*16+i]);
+				OLED_SetPos(x,y+1);
+				for(i=0;i<8;i++)
+					WriteDat(F8X16[c*16+i+8]);
+				x += 8;
+				j++;
+			}
+		}break;
+	}
+}
+
+//--------------------------------------------------------------
+// Prototype      : void OLED_ShowCN(unsigned char x, unsigned char y, unsigned char N)
+// Calls          :
+// Parameters     : x,y -- ��ʼ������(x:0~127, y:0~7); N:������codetab.h�е�����
+// Description    : ��ʾcodetab.h�еĺ���,16*16����
+//--------------------------------------------------------------
+void OLED_ShowCN(unsigned char x, unsigned char y, unsigned char N)
+{
+	unsigned char wm=0;
+	unsigned int  adder=32*N;
+	OLED_SetPos(x , y);
+	for(wm = 0;wm < 16;wm++)
+	{
+		WriteDat(F16x16[adder]);
+		adder += 1;
+	}
+	OLED_SetPos(x,y + 1);
+	for(wm = 0;wm < 16;wm++)
+	{
+		WriteDat(F16x16[adder]);
+		adder += 1;
+	}
+}
+
+//--------------------------------------------------------------
+// Prototype      : void OLED_DrawBMP(unsigned char x0,unsigned char y0,unsigned char x1,unsigned char y1,unsigned char BMP[]);
+// Calls          :
+// Parameters     : x0,y0 -- ��ʼ������(x0:0~127, y0:0~7); x1,y1 -- ���Խ���(������)������(x1:1~128,y1:1~8)
+// Description    : ��ʾBMPλͼ
+//--------------------------------------------------------------
+void OLED_DrawBMP(unsigned char x0,unsigned char y0,unsigned char x1,unsigned char y1,unsigned char BMP[])
+{
+	unsigned int j=0;
+	unsigned char x,y;
+
+  if(y1%8==0)
+		y = y1/8;
+  else
+		y = y1/8 + 1;
+	for(y=y0;y<y1;y++)
+	{
+		OLED_SetPos(x0,y);
+    for(x=x0;x<x1;x++)
+		{
+			WriteDat(BMP[j++]);
+		}
+	}
+}
+
+
+int main(void) {
+
+  printf("%s %s \n", __DATE__, __TIME__);
+  printf("OLED demo test..\n");
+
+  i2c_bus = metal_i2c_get_device(0);
+
+  if (i2c_bus == NULL) {
+    printf("I2C not available \n");
+    return RET_NOK;
+  }
+
+  metal_i2c_init(i2c_bus, I2C_BAUDRATE, METAL_I2C_MASTER);
+
+  OLED_Init();
+
+
+  /* Loop and print data from slaves every 1s */
+  while (1) {
+
+	  OLED_Fill(0xFF);//È«ÆÁµãÁÁ
+	  delay_ms(2000);
+
+	  OLED_Fill(0x00);//È«ÆÁÃð
+	  delay_ms(2000);
+
+	  for(int i=0;i<5;i++)
+	  {
+		  OLED_ShowCN(22+i*16,0,i);//²âÊÔÏÔÊ¾ÖÐÎÄ
+	  }
+	  delay_ms(2000);
+
+	  OLED_ShowStr(0,3,"SparkFun Red-V Board",1);//²âÊÔ6*8×Ö·û
+	  OLED_ShowStr(0,4,"Hello Funpack",2);				//²âÊÔ8*16×Ö·û
+	  delay_ms(2000);
+
+	  OLED_CLS();//ÇåÆÁ
+	  OLED_OFF();//²âÊÔOLEDÐÝÃß
+	  delay_ms(2000);
+
+	  OLED_ON();//²âÊÔOLEDÐÝÃßºó»½ÐÑ
+	  OLED_DrawBMP(0,0,128,8,(unsigned char *)BMP1);//²âÊÔBMPÎ»Í¼ÏÔÊ¾
+	  delay_ms(2000);
+  }
+
+  return RET_OK;
+}
